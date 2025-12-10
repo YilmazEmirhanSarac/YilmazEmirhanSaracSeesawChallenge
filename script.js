c8bdd2c3320b187a08b91b5a367048564286653f
Yılmaz Emirhan Saraç <yilmazemirhansarac@icloud.com>
2025-12-10 17:59:46 +0300

    Implement click event and coordinate calculation logic

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Project connected")
+
+const plank = document.getElementById('plank');
+
+plank.addEventListener('click', function(event) {
+
+    const rect = plank.getBoundingClientRect();
+
+    const positionFromLeft = event.clientX - rect.left;
+    
+    console.log("Distance from letf side (px)", positionFromLeft);
+})
